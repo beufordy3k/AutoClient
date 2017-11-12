c619305de65046d70001fcea7d2bd34f636bc93c
Rufus Harvey <Rufus Harvey>
2017-11-12 12:13:29 -0500

    Add project files.

---
+++ AutoClient/VAutoClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+namespace VAutoClient
+{
+    public class VAutoClient
+    {
+        private ConcurrentDictionary<string, DealersResponse> Dealers { get; set; }
+        private ConcurrentQueue<string> Vehicles { get; set; }
+        
+
+        public void Run()
+        {
+            // 1. Get Vehicles
+            // 2. Get Vehicle Detail
+            // 3. Get Dealer Info
+            // 4. Build Answer Response with Vehicles grouped by dealer
+
+
+        }
+    }
+}
