1933ccb59186d6961bd99d29611597cdeeeb6504
Rufus Harvey <rufushy3k@gmail.com>
2017-11-13 19:08:05 -0500

    Added grouping logic.

--- AutoClient/VAutoClient.cs
+++ AutoClient/VAutoClient.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 
 using Common.Logging;
@@ -15,9 +18,11 @@ namespace AutoClient
         private ILog Logger { get; }
         private HttpClient Client { get; }
 
-        private ConcurrentDictionary<string, DealersResponse> Dealers { get; } = new ConcurrentDictionary<string, DealersResponse>();
-        private ConcurrentQueue<int> VehiclesQueue { get; } = new ConcurrentQueue<int>();
-        private ConcurrentDictionary<int, VehicleResponse> Vehicles = new ConcurrentDictionary<int, VehicleResponse>();
+        private ConcurrentQueue<int> VehicleQueue { get; } = new ConcurrentQueue<int>();
+        private ConcurrentDictionary<int, VehicleResponse> Vehicles { get; } = new ConcurrentDictionary<int, VehicleResponse>();
+
+        private ConcurrentQueue<int> DealerQueue { get; } = new ConcurrentQueue<int>();
+        private ConcurrentDictionary<int, DealersResponse> Dealers { get; } = new ConcurrentDictionary<int, DealersResponse>();
 
         public VAutoClient(ILog logger, HttpClient client)
         {
@@ -63,7 +68,7 @@ namespace AutoClient
 
                 if (vehicleIdsResponse.VehicleIds == null || !vehicleIdsResponse.VehicleIds.Any())
                 {
-                    Logger.Error(l => l($"Error encountered retrieving Vehicle Id List from endpoint '{endpointUrl}', using datasetId '{datasetId}'. No Vehicles returned."));
+                    Logger.Error(l => l($"Error encountered retrieving Vehicle Id List from endpoint '{endpointUrl}', using Dataset Id '{datasetId}'. No Vehicles returned."));
 
                     return null; //No Results
                 }
@@ -71,7 +76,7 @@ namespace AutoClient
                 foreach (var id in vehicleIdsResponse.VehicleIds)
                 {
                     //Add Id to Vehicle Queue for processing - could be a TPL/other parallel mechanism for processing items.
-                    VehiclesQueue.Enqueue(id);
+                    VehicleQueue.Enqueue(id);
                 }
             }
             catch (VAutoException vaex)
@@ -82,35 +87,131 @@ namespace AutoClient
             }
 
             // 3. Get Vehicle Detail
-            while (VehiclesQueue.TryDequeue(out var vehicleId))
+            while (VehicleQueue.TryDequeue(out var vehicleId))
             {
+                var id = vehicleId;
+
+
+                if (Vehicles.ContainsKey(vehicleId))
+                {
+                    continue; //Vehicle detail already retrieved
+                }
+
+                VehicleResponse vehicleResponse;
+
                 try
                 {
-                    var vehicleResponse = await vehicleClient.GetVehicleAsync(datasetId, vehicleId);
-
-                    if (vehicleResponse == null)
-                    {
-                        var id = vehicleId;
-                        Logger.Error(l => l($"Error encountered retrieving Vehicle Detail from endpoint '{endpointUrl}', using datasetId '{datasetId}' and vehicle Id '{id}'. No Vehicle Detail returned. Continuing."));
-                        continue;
-                    }
-
-
-
+                    vehicleResponse = await vehicleClient.GetVehicleAsync(datasetId, vehicleId);
                 }
                 catch (VAutoException vaex)
                 {
-                    Logger.Error(l => l($"Exception encountered retrieving Vehicle Detail from endpoint '{endpointUrl}', using dataset Id '{datasetId}' and vehicle Id '{vehicleId}"), vaex);
-                    throw;
+                    Logger.Error(l => l($"Exception encountered retrieving Vehicle Detail from endpoint '{endpointUrl}', using Dataset Id '{datasetId}' and Vehicle Id '{id}"), vaex);
+
+                    continue; //Process next item in queue
                 }
+
+                if (vehicleResponse == null)
+                {
+                    Logger.Error(l => l($"Error encountered retrieving Vehicle Detail from endpoint '{endpointUrl}', using Dataset Id '{datasetId}' and Vehicle Id '{id}'. No Vehicle Detail returned. Continuing."));
+                    continue;
+                }
+
+                if (Vehicles.ContainsKey(vehicleId) || !Vehicles.TryAdd(vehicleId, vehicleResponse))
+                {
+                    Logger.Warn(l => l($"Vehicle Detail from endpoint '{endpointUrl}' already added. Dataset Id '{datasetId}', Vehicle Id '{id}'."));
+
+                    continue;
+                }
+
+                Logger.Debug(l => l($"Vehicle Detail added for Vehicle Id '{id}' from endpoint '{endpointUrl}', using Dataset Id '{datasetId}'"));
+
+                if (vehicleResponse.DealerId.HasValue)
+                {
+                    DealerQueue.Enqueue(vehicleResponse.DealerId.GetValueOrDefault());
+                }
+
             }
 
             // 4. Get Dealer Info
+            while (DealerQueue.TryDequeue(out var dealerId))
+            {
+                var id = dealerId;
+
+                if (Dealers.ContainsKey(dealerId))
+                {
+                    continue; //Dealer detail already retrieved
+                }
+
+                DealersResponse dealerResponse;
+
+                try
+                {
+                    dealerResponse = await dealersClient.GetDealerAsync(datasetId, dealerId);
+                }
+                catch (VAutoException vaex)
+                {
+                    Logger.Error(l => l($"Exception encountered retrieving Dealer Detail from endpoint '{endpointUrl}', using Dataset Id '{datasetId}' and Dealer Id '{id}"), vaex);
+                    throw;
+                }
+
+                if (dealerResponse == null)
+                {
+                    Logger.Error(l => l($"Error encountered retrieving Dealer Detail from endpoint '{endpointUrl}', using Dataset Id '{datasetId}' and Dealer Id '{id}'. No Vehicle Detail returned. Continuing."));
+                    continue;
+                }
+
+                if (Vehicles.ContainsKey(dealerId) || !Dealers.TryAdd(dealerId, dealerResponse))
+                {
+                    Logger.Debug(l => l($"Dealer Detail from endpoint '{endpointUrl}' already added. Dataset Id '{datasetId}', Dealer Id '{id}'."));
+
+                    continue;
+                }
+
+                Logger.Debug(l => l($"Dealer Detail added for Dealer Id '{id}' from endpoint '{endpointUrl}', using Dataset Id '{datasetId}'"));
+            }
+
+            var groupedVehicles = Vehicles.GroupBy(v => v.Value.DealerId); // This is *not* a good idea (memory usage and CPU) for large volume data sets
+
             // 5. Build Answer Response with Vehicles grouped by dealer
+            var answer = new Answer
+            {
+                Dealers = (ObservableCollection<DealerAnswer>) groupedVehicles
+                    .Where(gv => gv.Key.HasValue)
+                    .Select(gv => new DealerAnswer
+                    {
+                        DealerId = gv.Key,
+                        Name = GetDealerName(gv.Key, Dealers),
+                        Vehicles = (ObservableCollection<VehicleAnswer>) gv.Select(v =>
+                        {
+                            var vehicle = v.Value;
+
+                            return new VehicleAnswer
+                            {
+                                VehicleId = vehicle.VehicleId,
+                                Make = vehicle.Make,
+                                Model = vehicle.Model,
+                                Year = vehicle.Year,
+                            };
+                        })
+                    })
+            };
+
             // 6. Submit Answer
             // 7. Return AnswerResponse
 
             return null;
+        }
+
+        private static string GetDealerName(int? id, IReadOnlyDictionary<int, DealersResponse> dealers)
+        {
+            if (!id.HasValue)
+            {
+                return null;
+            }
+
+            var idValue = id.GetValueOrDefault();
+
+            return dealers.ContainsKey(idValue) ? dealers[idValue].Name : null;
         }
     }
 }
